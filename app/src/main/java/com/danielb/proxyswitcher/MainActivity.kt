e24947c9a2a65d8829104fdcfda05426af5e1a5e
Balázs Dániel <shockrifle666@gmail.com>
2018-10-06 18:36:49 +0200

    fixed notification, added icons

--- app/src/main/java/com/danielb/proxyswitcher/MainActivity.kt
+++ app/src/main/java/com/danielb/proxyswitcher/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.danielb.proxyswitcher
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startService(Intent(this, PacHostService::class.java))
+
+        //Start service:
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(Intent(this, PacHostService::class.java))
+        } else {
+            startService(Intent(this, PacHostService::class.java))
+        }
+
     }
 }
--- app/src/main/java/com/danielb/proxyswitcher/PacHostService.kt
+++ app/src/main/java/com/danielb/proxyswitcher/PacHostService.kt
@@ -1,19 +1,26 @@
 package com.danielb.proxyswitcher
 
-import android.app.Service
+import android.annotation.TargetApi
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
 import android.util.Log
 import java.io.IOException
+
 
 class PacHostService : Service() {
     companion object {
         var isOn: Boolean = false
         val TAG = PacHostService::class.java.simpleName
+        const val CHANNEL_ID_FOREGROUND = "com.danielb.proxyswitcher.notification.CHANNEL_ID_FOREGROUND"
     }
 
     private var nano: NanoServer? = null
+    private var lastShownNotificationId = 0
 
     override fun onCreate() {
         Log.e(TAG, "service started")
@@ -34,6 +41,7 @@ class PacHostService : Service() {
         return null
     }
 
+
     override fun onDestroy() {
         super.onDestroy()
 
@@ -42,7 +50,6 @@ class PacHostService : Service() {
         isOn = false
     }
 
-
     private fun start() {
         try {
             nano?.start()
@@ -50,21 +57,60 @@ class PacHostService : Service() {
             e.printStackTrace()
         }
 
-        runAsForeground()
+        createAndShowForegroundNotification(this, 1)
     }
 
     fun stopAsForeground() {
         stopForeground(true)
     }
 
-    private fun runAsForeground() {
+    private fun createAndShowForegroundNotification(service: Service, notificationId: Int) {
 
-        val notification = NotificationCompat.Builder(this, "")
-                .setSmallIcon(R.drawable.ic_launcher_background)
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+
+        val builder = getNotificationBuilder(service, CHANNEL_ID_FOREGROUND, NotificationManagerCompat.IMPORTANCE_LOW)
+        builder.setOngoing(true)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_notification)
                 .setContentText("localhost:8080")
                 .setContentTitle("Server running...")
-                .setTicker("Server running...").build()
+                .setTicker("Server running...")
 
-        startForeground(1, notification)
+        val notification = builder.build()
+
+        service.startForeground(notificationId, notification)
+
+        if (notificationId != lastShownNotificationId) {
+            val nm = service.getSystemService(Activity.NOTIFICATION_SERVICE) as NotificationManager
+            nm.cancel(lastShownNotificationId)
+        }
+        lastShownNotificationId = notificationId
+    }
+
+    private fun getNotificationBuilder(context: Context, channelId: String, importance: Int): NotificationCompat.Builder {
+        val builder: NotificationCompat.Builder
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            prepareChannel(context, channelId, importance)
+        }
+        builder = NotificationCompat.Builder(context, channelId)
+        return builder
+    }
+
+    @TargetApi(26)
+    private fun prepareChannel(context: Context, id: String, importance: Int) {
+        val appName = context.getString(R.string.app_name)
+        val description = context.getString(R.string.notifications_channel_description)
+        val nm = context.getSystemService(Activity.NOTIFICATION_SERVICE) as? NotificationManager
+
+        if (nm != null) {
+            var nChannel: NotificationChannel? = nm.getNotificationChannel(id)
+
+            if (nChannel == null) {
+                nChannel = NotificationChannel(id, appName, importance)
+                nChannel.description = description
+                nm.createNotificationChannel(nChannel)
+            }
+        }
     }
 }
