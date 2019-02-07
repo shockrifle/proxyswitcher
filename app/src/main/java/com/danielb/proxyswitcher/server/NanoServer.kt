9d62870ebfe193e8b5261a6d88febd750fd2586f
Balázs Dániel <shockrifle666@gmail.com>
2019-02-07 09:19:47 +0100

    serving selected proxy

--- app/src/main/java/com/danielb/proxyswitcher/server/NanoServer.kt
+++ app/src/main/java/com/danielb/proxyswitcher/server/NanoServer.kt
@@ -1,12 +1,22 @@
 package com.danielb.proxyswitcher.server
 
+import android.text.TextUtils
+import android.util.Log
+import com.danielb.proxyswitcher.repository.ProxyRepository
 import fi.iki.elonen.NanoHTTPD
 import java.io.IOException
 
 class NanoServer @Throws(IOException::class) constructor() : NanoHTTPD(8080) {
 
+
     override fun serve(session: IHTTPSession): NanoHTTPD.Response {
-        val msg = "function FindProxyForURL(url, host) { return \"PROXY 10.64.65.133:8888;\" }"
+        val proxy = ProxyRepository.getProxyById(ProxyRepository.restoreSelection())
+        var host = "DIRECT"
+        if (!TextUtils.isEmpty(proxy?.host)) {
+            host = "PROXY ${proxy!!.host}"
+        }
+        Log.i(this.javaClass.simpleName, "Host served: $host")
+        val msg = "function FindProxyForURL(url, host) { return \"$host;\" }"
         return NanoHTTPD.newFixedLengthResponse("$msg\n")
     }
 }
